0a8b31a54725826581c7fbdd53ece85f921e9292
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2014-03-28 12:03:03 +0100

    GC

--- GC/solution.hs
+++ GC/solution.hs
@@ -1,21 +1,27 @@
-gc :: Fractional a => [Char] -> a
-gc base = (fromIntegral lg) / (fromIntegral l)
+import Data.List
+import Text.Printf
+
+gc :: [Char] -> Double
+gc base = 100 * (fromIntegral lg) / (fromIntegral l)
     where
         l = length base
         lg = length $ filter (\x -> x=='G'||x=='C') base
 
 -- I/O
-parseFastas :: [[a]] -> [([a], [a])]
+parseFastas :: [[Char]] -> [([Char], [Char])]
 parseFastas []         = []
-parseFastas (l1:l2:ls) = (id, bases) : (parseFastas ls)
+parseFastas (l1:ls) = (id, concat bases) : (parseFastas lx)
     where
         id = tail l1 -- Tail because the ">" character is not part of the ID.
-        bases = l2
-
+        bases = takeWhile (not.isId) ls
+        lx = drop (length bases) ls
+        isId ('>':_) = True
+        isId _       = False        
+    
 main :: IO ()
 main = do
     s <- getContents
     let fs = parseFastas $ lines s
     let (m, id) = maximum [ (gc base, i) | (i, base) <- fs ]
     putStrLn id
-    putStrLn $ show m  
+    printf "%.6f\n" m  
