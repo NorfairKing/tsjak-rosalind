72a1c4dd7f8223249dae6194aabab5694af342f4
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2014-04-02 17:10:29 +0200

    DEG, less ugly

--- algoritmic_heights/DEG/solution.hs
+++ algoritmic_heights/DEG/solution.hs
@@ -1,4 +1,4 @@
-import Data.List
+import Control.Monad
 
 degrees :: (Enum a, Num a, Ord a) => [(a, a)] -> [Int]
 degrees eds = [ (sum . map (count x)) eds | x <- [1..n] ]
@@ -15,10 +15,16 @@ count x (y,z) = fromEnum (x == y) + fromEnum (x == z)
 rd :: String -> Int
 rd x = read x :: Int
 
+getInts :: IO [Int]
+getInts = do
+    line <- getLine
+    return $ map rd $ words line
+
 main :: IO ()
 main = do
-    cs <- getContents
-    let edges = sort $ map (tuplify . map rd . words) . tail . lines$ cs 
+    [n,e] <- getInts
+    eds <- replicateM e getInts
+    let edges = map tuplify eds 
     putStrLn $ (unwords . map show . degrees) edges
     where
         tuplify [x,y] = (x,y)  
