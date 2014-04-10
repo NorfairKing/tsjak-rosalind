4f057df6e425a75ce5852558c1a40886bce6b7f3
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2014-04-10 10:51:56 +0200

    PAR and PAR3

---
+++ algoritmic_heights/PAR3/solution.hs
@@ -0,0 +1,24 @@
+partition :: Ord a => [a] -> [a]
+partition [] = []
+partition (x:xs) = filter (<x) xs ++ [x] ++ filter (==x) xs ++ filter (>x) xs
+
+
+-- IO
+rd :: String -> Int
+rd x = read x :: Int
+
+getInt :: IO Int
+getInt = do
+    line <- getLine
+    return $ rd line
+
+getInts :: IO [Int]
+getInts = do
+    line <- getLine
+    return $ map rd $ words line
+
+main :: IO ()
+main = do
+    _ <- getInt
+    xs <- getInts
+    putStrLn $ unwords $ map show $ partition xs
