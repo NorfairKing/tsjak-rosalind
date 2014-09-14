84f62f745e7cac1460bfb007dda3cc2d5a47b684
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2014-09-14 08:38:07 +0200

    3sum done

--- algoritmic_heights/3SUM/solution.hs
+++ algoritmic_heights/3SUM/solution.hs
@@ -1,13 +1,32 @@
 import Control.Monad
--- This is too slow, redo!
+import Data.Maybe
+import qualified Data.Map.Lazy as M
 
-threeSum :: [Int] -> String
-threeSum xs = if length ll >= 1
-            then (unwords . map show) $ head ll
-            else "-1"
-    where
-        ll = indices $ zip [1..] xs
-        indices xs = [ [p, q, r] | (p,x) <- xs, (q,y) <- xs, (r,z) <- xs, x + y + z == 0 , p < q, q < r ] 
+-- The famous three sum program, solved with an O(logN) lookup structure.
+threeSum :: [Int] -> Maybe (Int, Int, Int)
+threeSum xs 
+    = mHead $ filter isJust idc
+    where 
+        idc = map (\(z, xi, yi) -> case M.lookup (-z) (indexMap xs) of
+                                        Just zi -> Just (xi, yi, zi)
+                                        Nothing -> Nothing     ) poss
+        
+        poss = [ (x+y,xi,yi) | (x,xi) <- xsl, (y,yi) <- xsl ]
+        xsl = xsList xs
+
+-- The first Just element from a list, or Nothing.
+mHead :: [Maybe a] -> Maybe a
+mHead [] = Nothing
+mHead (Nothing:xs) = mHead xs
+mHead (mx@(Just x):_) = mx
+
+-- A map of elements to their indices in the original lest.
+indexMap :: [Int] -> M.Map Int Int
+indexMap xs = M.fromList $ xsList xs
+
+-- A list of the elements with their indices
+xsList :: [Int] -> [(Int, Int)]
+xsList xs = zip xs [1..]
 
 
 -- IO
@@ -19,8 +38,12 @@ getInts = do
     line <- getLine
     return $ map rd $ words line
 
+showResult :: Maybe (Int, Int, Int) -> String
+showResult Nothing = "-1"
+showResult (Just (xi,yi,zi)) = (unwords . map show) [xi,yi,zi]
+
 main :: IO ()
 main = do
     [k, _] <- getInts
     xss <- replicateM k getInts
-    putStrLn $ init $ unlines $ map threeSum xss
+    putStrLn $ init $ unlines $ map (showResult . threeSum) xss
